280401f3c2499189fccb4bc64d47f0eec2e8c622
Jon-Paul Lussier Jr <jonpaul.lussier@gmail.com>
2011-02-03 14:46:08 -0500

    Debug for initial deploy

--- config/environment.rb
+++ config/environment.rb
@@ -3,8 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 GsiCommunity::Application.initialize!
-
-Rails::Initializer.run do |config|
-  config.gem 'right_aws'
-  config.gem 'right_http_connection'
-end
